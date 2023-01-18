ff60f85b27c4a9a02d9887603b8065dbbaef05c5
ZiChun <zijie.zeng@foxmail.com>
2023-01-18 17:22:46 +0800

    支付模块-初始化

---
+++ cloud-provider-payment8001/src/main/java/com/zichun/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.zichun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zichunyoushan
+ * @version 1.0.0
+ * @date Created in 2023/1/18 17:13
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
