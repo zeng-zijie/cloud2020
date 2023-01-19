810e8afa5fb263ab3386478b0c43c5284f364b73
ZiChun <zijie.zeng@foxmail.com>
2023-01-19 12:58:23 +0800

    支付消费者模块-完成-已测试-v2

--- cloud-provider-payment8001/src/main/java/com/zichun/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/zichun/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.zichun.springcloud.entities.CommonResult;
 import com.zichun.springcloud.entities.Payment;
 import com.zichun.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -24,7 +21,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult<Integer> create(Payment payment) {
+    public CommonResult<Integer> create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("**** 插入结果: " + result + " ****");
         if (result > 0) {
