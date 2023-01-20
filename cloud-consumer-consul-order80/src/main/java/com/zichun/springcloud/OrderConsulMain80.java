2f97df36f1ed3cb4efe005441a62592801c620a6
ZiChun <zijie.zeng@foxmail.com>
2023-01-20 16:22:25 +0800

    整合了 Consul

---
+++ cloud-consumer-consul-order80/src/main/java/com/zichun/springcloud/OrderConsulMain80.java
@@ -0,0 +1,19 @@
+package com.zichun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zichunyoushan
+ * @version 1.0.0
+ * @date Created in 2023/1/20 15:53
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/zichun/springcloud/PaymentMain8006.java
@@ -0,0 +1,18 @@
+package com.zichun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zichunyoushan
+ * @version 1.0.0
+ * @date Created in 2023/1/20 15:06
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
