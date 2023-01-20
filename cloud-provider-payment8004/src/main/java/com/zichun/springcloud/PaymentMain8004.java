f7b0a948102d75b87b6772edbf6503d625760c06
ZiChun <zijie.zeng@foxmail.com>
2023-01-20 14:15:14 +0800

    整合了 Zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/zichun/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.zichun.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zichunyoushan
+ * @version 1.0.0
+ * @date Created in 2023/1/20 12:59
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/zichun/springcloud/controller/PaymentController.java
@@ -0,0 +1,27 @@
+package com.zichun.springcloud.controller;
+
+import cn.hutool.core.lang.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zichunyoushan
+ * @version 1.0.0
+ * @date Created in 2023/1/20 13:00
+ */
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk() {
+        return "springcloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+}
