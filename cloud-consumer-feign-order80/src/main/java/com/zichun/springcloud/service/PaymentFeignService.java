c3ab29162a109779f0303b6b484582c7b263927b
ZiChun <zijie.zeng@foxmail.com>
2023-01-29 19:34:25 +0800

    整合 open-feign

---
+++ cloud-consumer-feign-order80/src/main/java/com/zichun/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,23 @@
+package com.zichun.springcloud.service;
+
+import com.zichun.springcloud.entities.CommonResult;
+import com.zichun.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author zichunyoushan
+ * @version 1.0.0
+ * @date Created in 2023/1/29 17:39
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+
+}
